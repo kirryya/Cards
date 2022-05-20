60c4814f0df68ba54c04cba7e26018831f15b610
kirryya <kir1987@inbox.ru>
2022-05-21 00:57:54 +0300

    HW14

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {HashRouter} from "react-router-dom";
+import {Provider} from "react-redux";
+import {store} from "./store/store";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
@@ -10,7 +12,9 @@ const root = ReactDOM.createRoot(
 root.render(
     <React.StrictMode>
         <HashRouter>
-            <App/>
+            <Provider store={store}>
+                <App/>
+            </Provider>
         </HashRouter>
     </React.StrictMode>
 );
